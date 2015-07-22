299e90fad57956ae9ebae8e396f36701fb072197
nczeroshift <nczeroshift.dev@gmail.com>
2015-07-22 23:24:26 +0100

    curves and armature export

--- bxon-3d.py
+++ bxon-3d.py
@@ -1,3 +1,6 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
 # ##### BEGIN ZLIB LICENSE BLOCK #####
 #
 # Copyright (c) 2015 Luis F.Loureiro
@@ -35,13 +38,257 @@ bl_info = {
     "category": "Import-Export"}
     
 import bpy
-from bxon import *
 import struct, time, sys, os, math, codecs
+from mathutils import *
 from bpy_extras.io_utils import ExportHelper
 from bpy.props import *
 
-## Blender dictionary object entry     
-class bXMapEntry:
+
+## BXON python writer methods ##
+
+BXON_NIL        = 0
+BXON_STRING     = 1
+BXON_BOOLEAN    = 2
+BXON_INT        = 3
+BXON_LONG       = 4
+BXON_FLOAT      = 5
+BXON_DOUBLE     = 6
+BXON_BYTE       = 7
+
+BXON_LENGTH_8   = 0x00
+BXON_LENGTH_16  = 0x10
+BXON_LENGTH_32  = 0x20
+BXON_LENGTH_64  = 0x30
+
+BXON_OBJECT     = 0x00
+BXON_ARRAY      = 0x40
+BXON_MAP        = 0x80
+
+## File writing context
+class bxon_context(object):
+    def __init__(self,f):
+        self.file = f
+
+    def tell(self):
+        return self.file.tell()
+
+    def seek(self,p):
+        self.file.seek(p,0)
+
+    def write(self,p,v):
+        self.file.write(struct.pack(p,v))
+
+    def close(self):
+        self.file.close()
+
+    def lengthForNative(self,t):
+        if(t == BXON_FLOAT):
+            return 4
+        elif(t == BXON_INT):
+            return 4
+        elif(t == BXON_DOUBLE):
+            return 8
+        elif(t == BXON_LONG):
+            return 8
+        elif(t == BXON_BYTE):
+            return 1
+        elif(t == BXON_BOOLEAN):
+            return 1
+            
+    def writeNative(self,t,s,val):
+        if t == BXON_FLOAT:
+            if s == 1:
+                self.file.write(struct.pack("<f",val))
+            elif s == 2:
+                self.file.write(struct.pack("<2f",val[0],val[1]))
+            elif s == 3:
+                self.file.write(struct.pack("<3f",val[0],val[1],val[2]))
+            elif s == 4:
+                self.file.write(struct.pack("<4f",val[0],val[1],val[2],val[3]))
+        elif t == BXON_INT:
+            if s == 1:
+                self.file.write(struct.pack("<i",val))
+            elif s == 2:
+                self.file.write(struct.pack("<2i",val[0],val[1]))
+            elif s == 3:
+                self.file.write(struct.pack("<3i",val[0],val[1],val[2]))
+            elif s == 4:
+                self.file.write(struct.pack("<4i",val[0],val[1],val[2],val[3]))
+        elif(t == BXON_LONG):
+            self.write("<q",val)
+        elif(t == BXON_DOUBLE):
+            self.write("<d",val) 
+        elif(t == BXON_BOOLEAN):
+            self.write("<B",val) 
+        elif(t == BXON_BYTE):
+            self.write("<B",val) 
+
+## Native value writer            
+class bxon_native(object):
+    def __init__(self, t, v = None):
+        self.type = t
+        self.value = v
+
+    def write(self,ctx):
+        if self.type == BXON_FLOAT:
+            ctx.write("<B",BXON_FLOAT)
+            ctx.write("<f",self.value)  
+        elif self.type == BXON_INT:
+            ctx.write("<B",BXON_INT)
+            ctx.write("<i",self.value)   
+        elif self.type == BXON_NIL: 
+            ctx.write("<B",BXON_NIL)
+        elif self.type == BXON_STRING:
+            ctx.write("<B",BXON_STRING | BXON_LENGTH_32)
+            data = bytearray(self.value.encode('utf-8'));
+            ctx.write("<i",len(data))
+            for c in data:
+                ctx.file.write(struct.pack("<B",c))
+        elif self.type == BXON_BOOLEAN: 
+            ctx.write("<B",BXON_BOOLEAN) 
+            ctx.write("<B",self.value)    
+        elif self.type == BXON_LONG:
+            ctx.write("<B",BXON_LONG)
+            ctx.write("<q",self.value)
+        elif self.type == BXON_DOUBLE:
+            ctx.write("<B",BXON_DOUBLE)
+            ctx.write("<d",self.value)  
+        elif self.type == BXON_BYTE:
+            ctx.write("<B",BXON_BYTE)
+            ctx.write("<B",self.value)
+
+## Map container    
+class bxon_map(object):
+    def __init__(self,ctx=None):
+        self.parent = None
+        self.context = ctx
+        self.startPos = None
+        self.endPos = None
+        self.map = {}
+
+    def write(self,p=None):
+        if p != None:
+            self.parent = p;
+            self.context = p.context;
+        
+        if self.startPos == None:
+            self.context.write("<B",BXON_MAP|BXON_LENGTH_64)
+            self.context.write("<q",0)
+            self.endPos = self.startPos = self.context.tell()
+
+    def _update(self, pos = None):
+        if(pos == None):
+            pos = self.context.tell()
+        self.endPos = pos
+        if(self.parent != None ):
+            self.parent._update(pos);
+
+    def put(self, key, vObj):
+        self.write()
+        kObj = bxon_native(BXON_STRING,key);
+        kObj.write(self.context)
+        t = type(vObj)
+        if t is bxon_native:
+            vObj.write(self.context)
+        elif t is str:
+            bxon_native(BXON_STRING,vObj).write(self.context)
+        elif t is float:
+            bxon_native(BXON_FLOAT,vObj).write(self.context)
+        elif t is int:
+            bxon_native(BXON_INT,vObj).write(self.context)
+        elif t is bool:
+            bxon_native(BXON_BOOLEAN,vObj).write(self.context)
+        else:
+            self.map[key] = vObj;
+            vObj.write(self)
+
+        self._update();
+        
+        return vObj;
+
+    def flush(self):
+        for i in self.map:
+            self.map[i].flush();
+    
+        lenght = self.endPos - self.startPos;
+        self.context.seek(self.startPos-8);
+        self.context.write("<q",lenght)
+        self.context.seek(self.endPos);
+
+## Array container
+class bxon_array(object):
+    def __init__(self,ctx=None,nType=BXON_NIL,nCount=0,nStride=1):
+        self.parent = None
+        self.context = ctx
+        self.startPos = None
+        self.endPos = None
+        self.nativeType = BXON_NIL;
+        self.array = []
+        self.stride = nStride
+        
+        if nType != BXON_NIL and nType != BXON_STRING:
+            self.nativeType = nType
+            self.nativeCount = nCount
+            self.nativeIndex = 0
+
+    def write(self,p=None):
+        if p != None:
+            self.parent = p;
+            self.context = p.context;
+        if self.startPos == None:
+            self.context.write("<B",BXON_ARRAY|BXON_LENGTH_64|self.nativeType)
+            self.context.write("<q",0)
+            self.startPos = self.context.tell()
+            if self.nativeType != BXON_NIL:
+                ePos = self.startPos + self.nativeCount * self.stride * self.context.lengthForNative(self.nativeType)
+                self.context.seek(ePos)
+                
+    def _update(self, pos = None):
+        if pos == None:
+            pos = self.context.tell()
+        self.endPos = pos
+        if self.parent != None:
+            self.parent._update(pos);
+
+    def push(self, obj):
+        self.write()
+        if self.nativeType != BXON_NIL:    
+            pos = self.startPos + self.nativeIndex * self.stride * self.context.lengthForNative(self.nativeType)
+            
+            if pos!=self.context.tell():
+                self.context.seek(pos)
+            if type(obj) == bxon_native:
+                self.context.writeNative(self.nativeType,self.stride,obj.value)
+            else:
+                self.context.writeNative(self.nativeType,self.stride,obj)
+            
+            self.nativeIndex+=1
+            
+            if(self.nativeIndex >= self.nativeCount):
+                self._update()
+        else:
+            if type(obj) is bxon_native:
+                obj.write(self.context)
+            else:
+                self.array.append(obj);
+                obj.write(self)
+            self._update()
+        return obj
+
+    def flush(self):
+        for i in range(len(self.array)):
+            self.array[i].flush();
+
+        lenght = self.endPos - self.startPos;
+        self.context.seek(self.startPos-8);
+        self.context.write("<q",lenght)
+        self.context.seek(self.endPos);
+
+
+## Object indexing classes ##
+    
+## Entry object in the indexed map
+class bxMapEntry:
     # Stored data reference
     data = None
     # Sequence id number
@@ -58,8 +305,8 @@ class bXMapEntry:
         self.users = []
         self.tracks = None
 
-## Indexed Blender objects dictionary
-class bXMap:
+## Utility class to index Blender objects
+class bxMap:
     # Dictionary atribute.
     dictionary = {}
     
@@ -69,7 +316,7 @@ class bXMap:
     
     ## Add a element to the dictionary.
     def add(self, data, key = None):
-        d_e = bXMapEntry(data,len(self.dictionary))
+        d_e = bxMapEntry(data,len(self.dictionary))
         if (key):
             if (key not in self.dictionary):
                 self.dictionary[key] = d_e
@@ -103,8 +350,10 @@ class bXMap:
             vec[self.dictionary[i].id] = self.dictionary[i]
         return vec
 
-## Objects animation tracks enumeration
-def bXListAnimationData(obj):
+## Animation utility functions ##
+
+## Utility function to enumerate objects animation tracks
+def bxListAnimationData(obj):
     tracks = []
     
     if obj.animation_data != None:
@@ -112,7 +361,7 @@ def bXListAnimationData(obj):
             action = obj.animation_data.action
             range = action.frame_range
             name = action.name
-            gr = bXListActionCurves(action)
+            gr = bxListActionCurves(action)
             aflag = False
             if "bones" in gr: aflag = True
             rtrack = {"name":name,"strips":[{"range":range,"groups":gr}]}
@@ -127,7 +376,7 @@ def bXListAnimationData(obj):
                 for s in t.strips:  
                     action = s.action
                     range = action.frame_range
-                    gr = bXListActionCurves(action)
+                    gr = bxListActionCurves(action)
                     if "bones" in gr: aflag = True
                     tstrips.append({"range":range,"groups":gr})
                 rtrack = {"name":name,"strips":tstrips}
@@ -135,8 +384,9 @@ def bXListAnimationData(obj):
                 tracks.append(rtrack)
     return tracks
 
-## Animation curves enumeration 
-def bXListActionCurves(action):
+
+## Utility function to enumerate animation curves  
+def bxListActionCurves(action):
     k_pos = None 
     k_rot = None 
     k_quat = None
@@ -203,18 +453,21 @@ def bXListActionCurves(action):
     if len(k_bones) > 0: ret["bones"] = k_bones
     return ret
 
-class bXExporter:
+
+## BXON exporter ##
+
+class bxExporter:
 
     ## Constructor.
     def __init__(self):
-        self.objectMap = bXMap()
-        self.meshMap = bXMap()
-        self.materialMap = bXMap()
-        self.textureMap = bXMap()
-        self.lampMap = bXMap()
-        self.armatureMap = bXMap()
-        self.cameraMap = bXMap()
-        self.curveMap = bXMap()
+        self.objectMap = bxMap()
+        self.meshMap = bxMap()
+        self.materialMap = bxMap()
+        self.textureMap = bxMap()
+        self.lampMap = bxMap()
+        self.armatureMap = bxMap()
+        self.cameraMap = bxMap()
+        self.curveMap = bxMap()
     
     ## Get unique selected elements.
     def getSelected(self):                
@@ -260,7 +513,7 @@ class bXExporter:
             if(add_obj):
                 self.objectMap.add(o)
                 element = self.objectMap.find(o.name)
-                atracks = bXListAnimationData(o)
+                atracks = bxListAnimationData(o)
                 if len(atracks) > 0:
                     element.tracks = atracks
       
@@ -407,7 +660,8 @@ class bXExporter:
         return True
         
     ## Write curve data.
-    def exportCurve(self, pNode, curve):
+    def exportCurve(self, pNode, entry):
+        curve = entry.data
         print("  Curve : \"" + curve.name + "\"")
         node = pNode.put(curve.name, bxon_map())
         node.put("resolution", curve.resolution_u)
@@ -430,7 +684,7 @@ class bXExporter:
         print("  Armature : \"" + arm.name + "\"")
         node = map.put(arm.name, bxon_map())
         
-        bonesMap = bXMap()
+        bonesMap = bxMap()
             
         for b_k in arm.bones.keys():
             bonesMap.add(None,arm.bones[b_k])
@@ -457,11 +711,11 @@ class bXExporter:
             head = b.head_local-bp_tail
             tail = b.tail_local-bp_tail
             
-            head = bNode.put("head", bxon_array(nType = BXON_FLOAT, nCount = 1, nStride = 3))
-            head.push(head)
+            h = bNode.put("head", bxon_array(nType = BXON_FLOAT, nCount = 1, nStride = 3))
+            h.push(head)
 
-            tail = bNode.put("tail", bxon_array(nType=BXON_FLOAT, nCount = 1, nStride = 3))
-            tail.push(tail)
+            t = bNode.put("tail", bxon_array(nType=BXON_FLOAT, nCount = 1, nStride = 3))
+            t.push(tail)
             
         return True
         
@@ -695,12 +949,12 @@ def runExport(filename):
 
     root = bxon_map(ctx)
 
-    bX = bXExporter()
+    bx = bxExporter()
 
-    bX.getSelected()
+    bx.getSelected()
 
     print("\n Exporting")
-    bX.export(root)
+    bx.export(root)
     
     root.flush()
     f.close()
@@ -710,7 +964,7 @@ def runExport(filename):
     print("bxon-3d end")
 
 ###### EXPORT OPERATOR #######
-class Export_bxon(bpy.types.Operator, ExportHelper):
+class export_bxon(bpy.types.Operator, ExportHelper):
     '''Exports selected objects as bxon-3d file'''
     bl_idname = "bxon_export"
     bl_label = "Export"
@@ -742,7 +996,7 @@ class Export_bxon(bpy.types.Operator, ExportHelper):
             return self.execute(context)
 
 def menu_func(self, context):
-    self.layout.operator(Export_bxon.bl_idname, text="bxon-3d (.bxon)")
+    self.layout.operator(export_bxon.bl_idname, text="bxon-3d (.bxon)")
 
 def register():
     bpy.utils.register_module(__name__)
