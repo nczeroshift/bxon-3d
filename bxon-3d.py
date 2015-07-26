07786c8b9ef6ab1e7e407fa853d78661ca4509eb
nczeroshift <nczeroshift.dev@gmail.com>
2015-07-26 23:11:09 +0100

    tweaks
    faces id, faces uv and faces materials with less arrays

--- bxon-3d.py
+++ bxon-3d.py
@@ -831,48 +831,28 @@ class bxExporter:
                 for group in v.groups:
                     mVW.push(bxon_native(BXON_INT,group.group))
                     mVW.push(bxon_native(BXON_FLOAT,group.weight))  
-           
-        if f3Count > 0:
-            mF3 = node.put("faces3",bxon_array(nType=BXON_INT,nCount = f3Count, nStride = 3))
-            for i,fc in enumerate(mesh.polygons):
-                vLen = len(fc.vertices)
-                if vLen == 3:
-                    mF3.push(fc.vertices)            
-            
-            if matCount > 1:
-                mF3m = node.put("faces3mat",bxon_array(nType=BXON_INT, nCount = f3Count ))
-                for i,fc in enumerate(mesh.polygons):
-                    if len(fc.vertices) == 3:
-                        mF3m.push(fc.material_index)
+        
+        mFaces = node.put("faces_vertices",bxon_array(nType=BXON_INT,nCount = (f3Count + f4Count), nStride = 4))
+        for i,fc in enumerate(mesh.polygons):
+            if len(fc.vertices) == 3:
+                mFaces.push([fc.vertices[0],fc.vertices[1],fc.vertices[2],-1])
+            elif len(fc.vertices) == 4:
+                mFaces.push(fc.vertices)
 
-            if uvCount > 0:
-                mF3uv = node.put("faces3uv",bxon_array(nType=BXON_FLOAT, nCount = f3Count * uvCount * 3, nStride = 2))
-                for i,fc in enumerate(mesh.polygons):
-                    if len(fc.vertices) == 3:
-                        for k,v in enumerate(fc.vertices):
-                            for j in range(uvCount):
-                                mF3uv.push(mesh.uv_layers[j].data[fc.loop_indices[k]].uv)
-         
-        if f4Count > 0:
-            mF4 = node.put("faces4",bxon_array(nType=BXON_INT, nCount = f4Count, nStride = 4))
+        if matCount > 1:
+            mFm = node.put("faces_materials",bxon_array(nType=BXON_INT,nCount = (f3Count + f4Count)))
             for i,fc in enumerate(mesh.polygons):
-                vLen = len(fc.vertices)
-                if vLen == 4:
-                    mF4.push(fc.vertices) 
-            
-            if matCount > 1:
-                mF4m = node.put("faces4mat",bxon_array(nType=BXON_INT, nCount = f4Count ))
+                if len(fc.vertices) == 3 or len(fc.vertices) == 4:
+                    mFm.push(fc.material_index)
+
+        if uvCount > 0:
+                mFuv = node.put("faces_uv",bxon_array(nType=BXON_FLOAT, nCount = uvCount * (f3Count * 3 + f4Count * 4), nStride = 2))
                 for i,fc in enumerate(mesh.polygons):
-                    if len(fc.vertices) == 4:
-                        mF4m.push(fc.material_index)
-                        
-            if uvCount > 0:
-                mF4uv = node.put("faces4uv",bxon_array(nType=BXON_FLOAT, nCount = f4Count * uvCount * 4, nStride = 2))
-                for i,fc in enumerate(mesh.polygons):
-                    if len(fc.vertices) == 4:
+                    if len(fc.vertices) == 3 or len(fc.vertices) == 4: 
                         for k,v in enumerate(fc.vertices):
                             for j in range(uvCount):
-                                mF4uv.push(mesh.uv_layers[j].data[fc.loop_indices[k]].uv)
+                                mFuv.push(mesh.uv_layers[j].data[fc.loop_indices[k]].uv)
+
         return True
                      
     def export(self, pNode):
